cad19503cf50c53f9698769610332cbedf5d47a8
Alex Baumgertner <alex.baumgertner@gmail.com>
2021-06-24 22:53:55 +0300

    Generate separate cert for admin panel

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(easy-deep-learning.org www.easy-deep-learning.org)
+domains=(easy-deep-learning.org www.easy-deep-learning.org admin.easy-deep-learning.org www.admin.easy-deep-learning.org)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="easy.deep.learning@gmail.com" # Adding a valid address is strongly recommended
